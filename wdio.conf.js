dad0ecf29e2ea7c74b4ce64e4cf5fc708ba29bed
joshzamor <josh.zamor@villagereach.org>
2017-12-29 11:22:30 -0600

    OLMIS-2884, first step to restructure around features

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,13 +1,9 @@
 // first, some pre-work to set variables for our later wdio.config.js
 
 // workaround from https://github.com/webdriverio/wdio-cucumber-framework/issues/79
-const stepsFolder = './src/steps';
-const fs = require('fs');
-var steps = [];
-fs.readdirSync(stepsFolder).forEach(file => {
-  step = stepsFolder + '/' + file;
-  steps.push(step);
-});
+const glob = require('glob');
+const steps = glob.sync('src/steps/*.steps.js');
+const features = glob.sync('src/features/**/*.feature');
 
 // configure proxy usage based on env setting - workaround for Taurus not supporting multiple
 // wdio.conf.js files
@@ -48,9 +44,7 @@ exports.config = {
     // then the current working directory is where your package.json resides, so
     // `wdio` will be called from there.
     //
-    specs: [
-        './src/features/*.feature',
-    ],
+    specs: features,
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
