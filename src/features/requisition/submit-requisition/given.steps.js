12b8331d75f94137de521882d558b6dfda6a90fa
Paulina Mironiuk <pmironiuk@soldevelo.com>
2018-11-06 10:36:48 +0100

    OLMIS-4573 updated given steps for submitting a requisition

--- src/features/requisition/submit-requisition/given.steps.js
+++ src/features/requisition/submit-requisition/given.steps.js
@@ -3,23 +3,27 @@ import { defineSupportCode } from 'cucumber';
 import ViewRequisitionPage from '../../../pages/requisition/view.requisition.page';
 import InitiateRequisitionPage from '../../../pages/requisition/initiate.requisition.page';
 import clickButton from '../../../support/action/clickButton';
+import waitForVisible from '../../../support/action/waitForVisible';
 import chooseSelectOption from '../../../support/action/chooseSelectOption';
 
 import '../../login/given.steps';
 
 defineSupportCode(({ Given }) => {
     Given(
-        /^I have initiated a requisition for "([^"]*)?" program and "([^"]*)?" period$/,
-        (program, period) => {
+        /^I have initiated a requisition for "([^"]*)?" program$/,
+        (program) => {
             InitiateRequisitionPage.open();
             InitiateRequisitionPage.waitForIsVisible();
             chooseSelectOption("Program", program);
             clickButton("Search");
+            browser.saveScreenshot("./screenshot4.png");
             setTimeout(() => {
                 browser.saveScreenshot("./screenshot5.png");
             }, 20000);
-            InitiateRequisitionPage.waitForTable();
+            waitForVisible('table tr td');
+            browser.saveScreenshot("./screenshot6.png");
             clickButton("Proceed");
+            browser.saveScreenshot("./screenshot7.png");
             ViewRequisitionPage.waitForIsVisible();
         }
     );
