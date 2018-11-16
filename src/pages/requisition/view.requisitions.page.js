81d4f7e99c5f842b8d32123583d925be8e2ec840
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-11-16 14:47:41 +0100

    OLMIS-5712: Fixed issue with entering View Requisitions page

--- src/pages/requisition/view.requisitions.page.js
+++ src/pages/requisition/view.requisitions.page.js
@@ -1,74 +1,60 @@
 import Page from '../page';
 import waitForVisible from '../../support/action/waitForVisible';
-import checkInURLPath from '../../support/check/checkInURLPath';
+import Button from '../../components/button';
 
 /**
  * View Requisitions Page object represents the related view in OpenLMIS UI.
  */
 class ViewRequisitionsPage extends Page {
 
-  /**
-  * Get this search button.
-  */
-  get search() {
-    return browser.element('input[type="submit"][value="Search"]');
-  }
+    constructor() {
+        super({
+            header: 'View Requisitions',
+            uri: 'requisitions/view'
+        });
+    }
 
-  /**
-   * Open the view requisitions page.
-   */
-  open() {
-    browser.execute(() => $('.navbar a:contains("Requisition")').parent().find('a:contains("View")').click());
-  }
+    /**
+     * Get this search button.
+     */
+    get searchButton() {
+        return new Button('Search');
+    }
 
-  /**
-   * Click to search.
-   */
-  clickSearch() {
-    this.search.click();
-  }
+    /**
+     * Click to search.
+     */
+    clickSearch() {
+        this.searchButton.click();
+    }
 
-  /**
-   * Wait for this page to be visible.
-   */
-  waitForIsVisible() {
-    waitForVisible('.navbar');
-  }
+    /**
+     * Click on 'View requisition' button for the given requisition.
+     * 
+     * @param {String} program  Program name.
+     * @param {String} period  Period name.
+     */
+    viewRequisition(program, period) {
+        browser.execute(function(program, period) {
+            $('table tr')
+                .filter((index, element) => {
+                    var that = $(element),
+                        programCell = that.find('td:nth-child(1)').text(),
+                        periodCell = that.find('td:nth-child(3)').text();
 
-  /**
-   * Is this page visibile?
-   */
-  isVisible() {
-    checkInURLPath(false, '#!/requisitions/view');
-  }
+                    return program === programCell && period === periodCell;
+                })
+                .find('td:nth-child(10) button:nth-child(1)')
+                .click();
+        }, program, period);
+    }
 
-  /**
-   * Click on 'View requisition' button for the given requisition.
-   * 
-   * @param {String} program  Program name.
-   * @param {String} period  Period name.
-   */
-  viewRequisition(program, period) {
-    browser.execute(function (program, period) {
-      $('table tr')
-        .filter((index, element) => {
-          var that = $(element),
-            programCell = that.find('td:nth-child(1)').text(),
-            periodCell = that.find('td:nth-child(3)').text();
-
-          return program === programCell && period === periodCell;
-        })
-        .find('td:nth-child(10) button:nth-child(1)')
-        .click();
-    }, program, period);
-  }
-
-  /**
-  * Wait for the table to be visible.
-  */
-  waitForTable() {
-    waitForVisible('//table/tbody/tr/td');
-  }
+    /**
+     * Wait for the table to be visible.
+     */
+    waitForTable() {
+        waitForVisible('//table/tbody/tr/td');
+    }
 }
 
 export default new ViewRequisitionsPage();
