5e4a7ee625179e71df2140d5d9b20b0973041cc6
Łukasz Lewczyński <llewczynski@soldevelo.com>
2018-12-12 14:23:45 +0100

    OLMIS-5755: Use table component in SupplyPartnerListPage

--- src/pages/administration/supply.partner.list.page.js
+++ src/pages/administration/supply.partner.list.page.js
@@ -1,6 +1,6 @@
 import Page from '../../components/page';
 import Button from '../../components/button';
-import waitForVisible from '../../support/action/waitForVisible';
+import Table from '../../components/table';
 
 /**
  * Supply Partner List Page object represents the related view in OpenLMIS UI.
@@ -16,25 +16,27 @@ class SupplyPartnerListPage extends Page {
         });
     }
 
+    get table() {
+        return new Table();
+    }
+
+    get addSupplyPartnerButton() {
+        return new Button('Add Supply Partner');
+    }
+
     /**
      * Wait for the supply partner to be visible in the table.
      */
     waitForSupplyPartner(code, name, hidden) {
-        waitForVisible(
-            `//td[text()="${code}"]/following-sibling::td[text()="${name}"]`,
-            hidden
-        );
+        this.table.waitFor([code, name], hidden);
     }
 
     clickEdit(code) {
-        new Button(
-            'Edit',
-            `//td[normalize-space(text())="${code}"]/following-sibling::td/button[text()="Edit"]`
-        ).click();
+        this.table.click([code], 3, 'Edit');
     }
 
     clickAddSupplyPartner() {
-        new Button('Add Supply Partner').click();
+        this.addSupplyPartnerButton.click();
     }
 }
 
