2bd5b6b4ae2e82c6f2bb7d0254472118b9ca5ada
Pawel Cieszko <pcieszko@soldevelo.com>
2019-05-08 13:01:39 +0200

    Adjusted webdriver resolution to 1600x900

--- wdio.conf.js
+++ wdio.conf.js
@@ -233,8 +233,8 @@ exports.config = {
         global.should = chai.should();
 
         browser.windowHandleSize({
-            width: 1920,
-            height: 1080
+            width: 1600,
+            height: 900
         });
     },
     //
