7c318ad813c543dc87052c4e1f7c4ca3ceae2d2d
saleksandra <soltys.ola@gmail.com>
2020-12-01 09:40:53 +0100

    OLMIS-7033: Defined  performance-report script in package.json

--- convertPerformanceResult.sh
+++ convertPerformanceResult.sh
@@ -4,4 +4,4 @@ sudo chmod 777 -R build/performanceResults
 sudo chmod 777 -R resources/assets
 
 sudo cp -vr resources/assets/ build/performanceResults/
-sudo node node_modules/htmlify-csv convert build/performanceResults/StepPerformanceResults.csv --output=build/performanceResults/StepPerformanceResults.html
+yarn run performance-report
