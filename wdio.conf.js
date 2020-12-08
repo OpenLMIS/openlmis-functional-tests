94a99a8e50cf22ecbb0f96464722d98af222512e
Pawel Muchowski <pmuchowski@soldevelo.com>
2020-12-08 12:31:03 +0100

    OLMIS-7127: Change chromedriver version

--- wdio.conf.js
+++ wdio.conf.js
@@ -121,6 +121,10 @@ let stepObjects = [];
 let scenarioObjects = [];
 let featureObject = {};
 
+const drivers = {
+    chrome: { version: '86.0.4240.22' },
+};
+
 // wdio config
 exports.config = {
     //
@@ -235,7 +239,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They
     // enhance your test setup with almost no effort. Unlike plugins, they don't
     // add new commands. Instead, they hook themselves up into the test process.
-    services: ['devtools', 'selenium-standalone'],
+    services: ['devtools', ['selenium-standalone', { installArgs: { drivers }, args: { drivers } }]],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -319,7 +323,7 @@ exports.config = {
         }
         fs.mkdirSync(recordingsDir);
         fs.mkdirSync(consoleLogDir);
-      },
+    },
     //
     // Gets executed before test execution begins. At this point you can access
     // all global variables, such as `browser`. It is the perfect place to
