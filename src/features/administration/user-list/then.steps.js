e4994f9c0ed6a4296ed24f640073c02576bf713e
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-12-09 16:08:37 +0100

    OLMIS-7127: Replaced omitted defineSupportCode

--- src/features/administration/user-list/then.steps.js
+++ src/features/administration/user-list/then.steps.js
@@ -1,17 +1,13 @@
-import { defineSupportCode } from 'cucumber';
+var {Then} = require('cucumber');
 
 import UserListPage from '../../../pages/administration/user.list.page';
 
-defineSupportCode(({ Then }) => {
+Then(
+    /^I should be brought to the user list page$/,
+    () => UserListPage.waitForIsVisible()
+);
 
-    Then(
-        /^I should be brought to the user list page$/,
-        () => UserListPage.waitForIsVisible()
-    );
-
-    Then(
-        /^I should see sorted user list by "([^"]*)?"$/,
-        sortOption => UserListPage.sortedBy(sortOption)
-    );
-
-});
+Then(
+    /^I should see sorted user list by "([^"]*)?"$/,
+    sortOption => UserListPage.sortedBy(sortOption)
+);
